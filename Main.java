872f9f74b89c72b7533cf989cc944af48829b7f4
Adrian <adrmalec@proton.me>
2024-05-04 12:37:24 +0200

    chore:zadanie4

--- Main.java
+++ Main.java
@@ -1,14 +1,37 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
         
-        int result = MathUtils.add(4, 30);
-        System.out.println("Wynik dodawania to: " + result);
+        Person[] people = new Person[5];
 
-        EmailMessenger emailMessenger = new EmailMessenger();
+        
+        final int b = 10;
 
-        emailMessenger.sendMessage("Wynik dodawania to: " + result);
+        try {
+            
+            people[0] = new Person("Jack Grealish", 26);
+            people[1] = new Person("Leroy Sane", 24);
+            people[2] = new Person("Jude Bellingham", 21);
+            people[3] = new Person("Anthony Davies", 25);
+            people[4] = new Person("Erling Halland", 20);
+
+            
+            for (Person person : people) {
+                int value = MathUtils.add(person.getAge(), b);
+                System.out.println("wartosc tej osoby " + person.getName() + ": " + value);
+
+               
+                EmailMessenger emailMessenger = new EmailMessenger();
+
+                
+                emailMessenger.sendMessage("wartosc tej osoby " + person.getName() + " to: " + value);
+            }
+        } catch (InvalidAgeException e) {
+            System.out.println("nieznany wiek: " + e.getMessage());
+        }
     }
 }
